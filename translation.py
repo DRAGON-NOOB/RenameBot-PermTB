40c833e3224b237b5af127302f3173315fde50fa
DRAGON-NOOB <69449962+DRAGON-NOOB@users.noreply.github.com>
2020-08-24 19:48:12 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -25,6 +25,4 @@ class Translation(object):
     
 1. Send me any Telegram File.
 2. Reply to that message with /rename NEWNAME.EXTENSION"""
-    ABOUT_USER="""I AM RENAME BOT MADE BY TEAM
-   <a href="t.me/Zed1Projctz"> ZED1PROJECTZ</a>"""   
-
+    
