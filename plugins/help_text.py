c301853c5fa48c2c3a6cd6190b62339c35c90fb1
DRAGON-NOOB <69449962+DRAGON-NOOB@users.noreply.github.com>
2020-08-25 20:23:26 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -63,7 +63,7 @@ async def about(bot, update):
     TRChatBase(update.from_user.id, update.text, "/about")
     await bot.send_message(
         chat_id=update.chat.id,
-        text=Translation.UPGRADE_TEXT,
+        text=Translation.About_TEXT,
         parse_mode="html",
         reply_to_message_id=update.message_id,
         disable_web_page_preview=True
