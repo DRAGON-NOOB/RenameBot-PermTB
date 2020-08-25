d7eb560e55190873d3cd7afb6cdb554952bde99e
PROFESSOR <66514907+Adithyan1133-ctrl@users.noreply.github.com>
2020-08-25 16:22:13 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -68,4 +68,16 @@ async def upgrade(bot, update):
         reply_to_message_id=update.message_id,
         disable_web_page_preview=True
     )
+    
+    
+@pyrogram.Client.on_message(pyrogram.Filters.command(["about"]))
+async def start(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/about")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.ABOUT_TEXT,
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="⭕SUPPORT CHANNEL⭕", url="t.me/Zed1Projctz")]]),
+        reply_to_message_id=update.message_id
+    )    
   
